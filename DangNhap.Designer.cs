b36740f44cb52c7321d299ed9bc30bdab1292e26
HoThiQueLinh <quelinhho@gmail.com>
2021-10-04 13:42:21 +0700

    first commit

---
+++ DangNhap.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace LinhBlack
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btndangnhap = new System.Windows.Forms.Button();
+            this.btnboqua = new System.Windows.Forms.Button();
+            this.txtdangnhap = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu :";
+            // 
+            // btndangnhap
+            // 
+            this.btndangnhap.BackColor = System.Drawing.Color.Lime;
+            this.btndangnhap.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndangnhap.Location = new System.Drawing.Point(151, 235);
+            this.btndangnhap.Name = "btndangnhap";
+            this.btndangnhap.Size = new System.Drawing.Size(118, 34);
+            this.btndangnhap.TabIndex = 2;
+            this.btndangnhap.Text = "Đồng ý";
+            this.btndangnhap.UseVisualStyleBackColor = false;
+            this.btndangnhap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnboqua
+            // 
+            this.btnboqua.BackColor = System.Drawing.Color.Lime;
+            this.btnboqua.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnboqua.Location = new System.Drawing.Point(299, 235);
+            this.btnboqua.Name = "btnboqua";
+            this.btnboqua.Size = new System.Drawing.Size(119, 34);
+            this.btnboqua.TabIndex = 3;
+            this.btnboqua.Text = "Bỏ qua";
+            this.btnboqua.UseVisualStyleBackColor = false;
+            this.btnboqua.Click += new System.EventHandler(this.btnboqua_Click);
+            // 
+            // txtdangnhap
+            // 
+            this.txtdangnhap.Location = new System.Drawing.Point(265, 105);
+            this.txtdangnhap.Name = "txtdangnhap";
+            this.txtdangnhap.Size = new System.Drawing.Size(206, 22);
+            this.txtdangnhap.TabIndex = 4;
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(265, 160);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.PasswordChar = '*';
+            this.txtmatkhau.Size = new System.Drawing.Size(206, 22);
+            this.txtmatkhau.TabIndex = 5;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(538, 349);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.txtdangnhap);
+            this.Controls.Add(this.btnboqua);
+            this.Controls.Add(this.btndangnhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DangNhap";
+            this.Text = "DangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btndangnhap;
+        private System.Windows.Forms.Button btnboqua;
+        private System.Windows.Forms.TextBox txtdangnhap;
+        private System.Windows.Forms.TextBox txtmatkhau;
+    }
+}
---
+++ DangNhap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinhBlack
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tendn = "ndungithue";
+            string matkhau = "Abc@123";
+            if (tendn.Equals(txtdangnhap.Text) && matkhau.Equals(txtmatkhau.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+                MessageBox.Show("Đăng nhập thất bại");
+        }
+
+        private void btnboqua_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
